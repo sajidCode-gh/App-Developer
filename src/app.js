3a78336080a4682976512bda4a62ae66158fa2b2
Muhammad Sajid <muhammadsajidkhan616@gmail.com>
2022-01-30 16:07:36 +0500

    hide navbar on scroll down

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 const dots = document.querySelector(".menu-icon");
 const nav = document.querySelector("nav");
 const navItems = document.querySelectorAll("nav li");
+const header = document.querySelector("header");
 
-dots.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  console.log("works");
-});
-
-navItems.forEach((item) => {
-  item.addEventListener("click", () => {
+if (window.innerWidth < 660) {
+  dots.addEventListener("click", () => {
     nav.classList.toggle("active");
+    header.classList.add("adjust-height");
   });
+
+  navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  });
+}
+
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  let currentScroll = document.body.scrollTop;
+  if (currentScroll > lastScroll && currentScroll > header.offsetHeight) {
+    lastScroll = currentScroll;
+    header.classList.add("hide");
+  } else {
+    lastScroll = currentScroll;
+    header.classList.remove("hide");
+  }
 });
